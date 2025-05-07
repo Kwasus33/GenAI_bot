95be5a3d58629ce9efafebec07117766f2c4935a
Jakub Kwaśniak <kubak04@outlook.com>
2025-05-08 01:40:57 +0200

    Extends llm parser by saving context and loading initial prompt

--- src/llm_parser.py
+++ src/llm_parser.py
@@ -1,7 +1,10 @@
 from google import genai
 from google.genai import types
-import os
 from dotenv import load_dotenv
+import os
+import re
+import json
+
 
 load_dotenv()
 
@@ -10,12 +13,45 @@ class LLM_Parser:
     def __init__(self):
         self.context = {}
         self.client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+        self._init_prompt()
 
-    def call_gemini(self):
+    def _init_prompt(self):
+        try:
+            base_dir = os.path.dirname(os.path.abspath(__file__))
+        except NameError:
+            base_dir = os.path.join(os.getcwd(), "/src/")
+        prompt_path = os.path.join(base_dir, "initial_prompt.txt")
+
+        with open(prompt_path, "r") as fh:
+            self.initial_prompt = fh.read()
+
+    def _update_context(self, user_prompt, ai_response):
+        self.context[user_prompt] = ai_response
+
+    def call_gemini(self, user_prompt, form):
+
+        prompt = (
+            self.initial_prompt
+            + f"\nSession Context:\n{list(self.context.items())}\n"
+            + f"\nCurrent form state:\n{form.to_json()}\n"
+            + f"User: {user_prompt}\n"
+        )
+
         response = self.client.models.generate_content(
             model="gemini-2.0-flash",
             config=types.GenerateContentConfig(
                 system_instruction="You are an advanced user assistance. You have to help them fill out a helpdesk form. "
             ),
-            contents="Tell me a joke",
+            contents=prompt,
         )
+
+        self._update_context(user_prompt, response.text)
+
+        match = re.search(r"```json(.*?)```", response.text, re.DOTALL)
+        if not match:
+            raise RuntimeError("Failed to extract JSON from the Gemini response.")
+
+        json_text = match.group(1).strip()
+        cleaned_response = json.loads(json_text)
+
+        return cleaned_response
