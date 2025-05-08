ba5c3863a84c06015685d1cfebbfe4bfaaabe70e
Jakub Kwaśniak <kubak04@outlook.com>
2025-05-08 03:41:53 +0200

    Adds argparser for program

--- src/chat_session.py
+++ src/chat_session.py
@@ -12,12 +12,13 @@ def chat_session(save_to: str = None, history_len: int = None):
     if save_to:
         with open(save_to, "w+") as fh:
             if history_len:
-               json.dump(obj=session["history"][:history_len], fp=fh)
-            else: json.dump(obj=session["history"], fp=fh)
+               json.dump(obj=session["history"][-history_len:], fp=fh, indent=4, ensure_ascii=False)
+            else: json.dump(obj=session["history"], fp=fh, indent=4, ensure_ascii=False)
 
-def start_session():
 
-    with chat_session() as session:
+def start_session(save_to: str = None, history_len: int = None):
+
+    with chat_session(save_to, history_len) as session:
 
         llm = LLM_Parser()
         form = Form()
@@ -27,7 +28,6 @@ def start_session():
         while not form.is_complete():
             print(f"\nCurrent form state: {form.to_json()}\n")
             user_input = input("You: ")
-            print(session)
             response = llm.call_gemini(user_input, form, session["history"])
             try:
                 response = json.loads(response)
--- src/main.py
+++ src/main.py
@@ -1,4 +1,29 @@
 from chat_session import start_session
+import argparse
+import os
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description="Chatbot Helpdesk AI: Start a chat session and optionally save it to a file."
+    )
+    parser.add_argument(
+        "--path",
+        type=str,
+        default=None,
+        required=False,
+        help="Path to json file to store chat session",
+    )
+    parser.add_argument(
+        "--length",
+        type=int,
+        default=None,
+        required=False,
+        help="Length of saved to file chat session",
+    )
+    return parser.parse_args()
+
 
 if __name__ == "__main__":
-    start_session()
+    args = parse_arguments()
+    start_session(args.path, args.length)
